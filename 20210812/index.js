a4c872ecb8f734393c3dc4a66d1d36f87475067f
sooraj <sahusooraj2619@gmail.com>
2021-08-12 23:12:27 +0530

    Array and string methods

---
+++ 20210812/index.js
@@ -0,0 +1,66 @@
+let  studentNames = ['Unique','Neelesh',67,'Ajay','Hemant',5,'Neha',1,'nainika',34,'Pooja','anita']
+// let  
+// for(let i=0;i<studentNames.length;i++) 
+// {
+//  let name = studentNames[i];
+//  console.log(  'My name is '+ name )
+// }
+
+
+
+// console.log(studentNames);
+
+// console.log(studentNames.pop()) //Pooja
+// console.log(studentNames);
+// console.log(studentNames.pop()) //Nainika
+// console.log(studentNames);
+// console.log(studentNames.pop())//Neha
+// console.log(studentNames);
+// console.log(studentNames.push('Sourabh')); // + Sourabh, 3
+// console.log(studentNames);
+// console.log(studentNames.push('Kritika'));
+// console.log(studentNames);
+// console.log(studentNames.push('Devendra'));
+
+
+// Js Documentation 
+
+/**
+ * This function is return the sum of two number which i passed into it
+ * @param {string} name  
+ * @param {number} age
+ * 
+ * @returns string
+ */
+
+// function showNameAndAge(name,age)
+// {
+//     let info = 'My name is ' + name + ' and Age is' + age
+//     // console.log(info)
+//     return info;
+// }
+
+//   let mydata = showNameAndAge('Raju',25)
+
+//   console.log( mydata + ', i am  studing web development' );
+
+
+// console.log(sumOfTwoNumber(2378,1000))
+
+// studentNames.forEach(name=>{  // For-Each loop
+
+//     console.log('My name is ' + name)
+// })
+
+// console.log( studentNames.sort()) // Sort age , sort name, date,size
+
+// let fruits = ['Mangos', 'Lichi' ,'Banana']
+
+// console.log(studentNames.concat(fruits,4))
+
+let arr1 =['a','g',1 , 7 , 8, 'o' ,'p', 'd']
+
+// let arr2 = arr1.slice(3)
+
+// console.log(arr1.slice(3,6));
+// console.log(studentNames); // Neelesh , Hemant ,Sourabh
---
+++ 20210812/strings.js
@@ -0,0 +1,9 @@
+let str = '   This is microtechnologies '
+
+console.log(str.toLocaleLowerCase())
+console.log(str.toLocaleUpperCase())
+console.log(str.toLowerCase())
+console.log(str.bold())
+console.log(str.concat(' and learning hear Web Dev'))
+console.log(str.replace(' is',' are'))
+console.log(str.trim())
